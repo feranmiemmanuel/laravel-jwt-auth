20787156782a06b0932d71cf8120a7c7179a05d4
feranmi <feranmiolasunkanmi91@gmail.com>
2022-09-21 00:57:43 +0100

    WOrking on jwt config

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
@@ -210,6 +211,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'JwtAuth' => PHPOpenSourceSaver\JWTAuth\Facades\JwtAuth::class,
+        'JwtAuthFactory' => PHPOpenSourceSaver\JWTAuth\Facades\JwtAuthFactory::class,
     ])->toArray(),
 
 ];
--- config/auth.php
+++ config/auth.php
@@ -14,10 +14,15 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
-
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
